4e1eea040076c01730a826312579623c968c7f0e
t4ccer <t4ccer@gmail.com>
2024-02-26 17:10:36 -0700

    Rename libs to not mention plutarch and drop unused ones

---
+++ plutus-context-builder/src/Plutus/ContextBuilder/Internal.hs
@@ -0,0 +1,20 @@
+module Plutus.ContextBuilder.Internal (
+  Normalizer (..),
+  mkNormalized,
+) where
+
+import Data.Kind (Type)
+import Plutus.ContextBuilder.Base (BaseBuilder, Builder, mkNormalizedBase)
+
+class Builder a => Normalizer (a :: Type) where
+  mkNormalized' :: a -> a
+
+instance Normalizer BaseBuilder where
+  mkNormalized' = mkNormalizedBase
+
+{- | Normalizes every value present in the builder structure.
+
+ @since 2.4.0
+-}
+mkNormalized :: Normalizer a => a -> a
+mkNormalized = mkNormalized'
